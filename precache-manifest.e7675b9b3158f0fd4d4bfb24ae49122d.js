db1b845ad9f9db0295a31ff6df64a152d91abcdd
vinodbalakumar <vinodbalakumar.p@gmail.com>
2021-06-12 23:51:39 +0530

    Updates

---
+++ precache-manifest.e7675b9b3158f0fd4d4bfb24ae49122d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0572fea2a60133e0914984ea4d46fd56",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "9a4689d5294bf497667e",
+    "url": "/shopping_cart/static/css/main.46a7486c.chunk.css"
+  },
+  {
+    "revision": "99a66e1e6e068d306b62",
+    "url": "/shopping_cart/static/js/2.49a76e25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping_cart/static/js/2.49a76e25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4689d5294bf497667e",
+    "url": "/shopping_cart/static/js/main.e070e5cb.chunk.js"
+  },
+  {
+    "revision": "817f0247a4835c870de9",
+    "url": "/shopping_cart/static/js/runtime-main.7e25c57b.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/shopping_cart/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/shopping_cart/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/shopping_cart/static/media/times-solid.2c5552a1.svg"
+  }
+]);
